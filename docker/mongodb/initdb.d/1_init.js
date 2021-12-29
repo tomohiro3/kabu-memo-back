4c8d6a9adf009e82196085f70540a70c8cd104f1
Tom <tomohiro3jp@gmail.com>
2021-12-30 01:43:21 +0900

    wIP: Making mongodb structure

---
+++ docker/mongodb/initdb.d/1_init.js
@@ -0,0 +1,13 @@
+db.movies.insertOne(
+  {
+    title: "The Favourite",
+    genres: [ "Drama", "History" ],
+    runtime: 121,
+    rated: "R",
+    year: 2018,
+    directors: [ "Yorgos Lanthimos" ],
+    cast: [ "Olivia Colman", "Emma Stone", "Rachel Weisz" ],
+    type: "movie"
+  }
+)
+
